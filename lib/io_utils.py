0acbee7a3b2094704eb796b176c1420958a72e10
brianfoo <brian@youaremyjoy.org>
2018-10-02 16:51:16 -0400

    update config, ioutils boilerplate

--- lib/io_utils.py
+++ lib/io_utils.py
@@ -1,43 +1,110 @@
 # -*- coding: utf-8 -*-
 
 import csv
+import gzip
+from netCDF4 import Dataset
+import numpy as np
 import os
+import zipfile
 
-def parseHeadings(arr, headings):
-    newArr = []
-    headingKeys = [key for key in headings]
-    for i, item in enumerate(arr):
-        newItem = {}
-        for key in item:
-            if key in headingKeys:
-                newItem[headings[key]] = item[key]
-        newArr.append(newItem)
-    return newArr
+def ascDump(filename):
+    with open(filename, 'rb') as f:
+        lines = list(f)
+        lines = lines[:6]
+        for line in lines:
+            print(line.strip())
 
-def parseNumber(string):
-    try:
-        num = float(string)
-        if "." not in string:
-            num = int(string)
-        return num
-    except ValueError:
-        return string
+def ncDump(filename, verb=True):
+    nc_fid = Dataset(filename, 'r')
+    def print_ncattr(key):
+        try:
+            print "\t\ttype:", repr(nc_fid.variables[key].dtype)
+            for ncattr in nc_fid.variables[key].ncattrs():
+                print '\t\t%s:' % ncattr,\
+                      repr(nc_fid.variables[key].getncattr(ncattr))
+        except KeyError:
+            print "\t\tWARNING: %s does not contain variable attributes" % key
 
-def parseNumbers(arr):
-    for i, item in enumerate(arr):
-        for key in item:
-            arr[i][key] = parseNumber(item[key])
-    return arr
+    # NetCDF global attributes
+    nc_attrs = nc_fid.ncattrs()
+    if verb:
+        print "NetCDF Global Attributes:"
+        for nc_attr in nc_attrs:
+            print '\t%s:' % nc_attr, repr(nc_fid.getncattr(nc_attr))
+    nc_dims = [dim for dim in nc_fid.dimensions]  # list of nc dimensions
+    # Dimension shape information.
+    if verb:
+        print "NetCDF dimension information:"
+        for dim in nc_dims:
+            print "\tName:", dim
+            print "\t\tsize:", len(nc_fid.dimensions[dim])
+            print_ncattr(dim)
+    # Variable information.
+    nc_vars = [var for var in nc_fid.variables]  # list of nc variables
+    if verb:
+        print "NetCDF variable information:"
+        for var in nc_vars:
+            if var not in nc_dims:
+                print '\tName:', var
+                print "\t\tdimensions:", nc_fid.variables[var].dimensions
+                print "\t\tsize:", nc_fid.variables[var].size
+                print_ncattr(var)
+    return nc_attrs, nc_dims, nc_vars
 
-def readCsv(filename, headings=False, doParseNumbers=True):
-    rows = []
-    if os.path.isfile(filename):
-        with open(filename, 'rb') as f:
-            lines = [line for line in f if not line.startswith("#")]
-            reader = csv.DictReader(lines, skipinitialspace=True)
-            rows = list(reader)
-            if headings:
-                rows = parseHeadings(rows, headings)
-            if doParseNumbers:
-                rows = parseNumbers(rows)
+def readCsv(filename, handler=None):
+    if not handler:
+        handler = open(filename, 'rb')
+    lines = list(handler)
+    rows = np.zeros((len(lines), len(lines[0].split(","))))
+    for i, line in enumerate(lines):
+        row = np.array([float(value) for value in line.split(",")])
+        rows[i] = row
+    handler.close()
     return rows
+
+def readNetCDF(filename, params):
+    ds = Dataset(filename, 'r')
+    valueKey = params["valueKey"]
+    zKey = None if "zKey" not in params else params["zKey"]
+    data = ds.variables[valueKey][:] if zKey is None else ds.variables[valueKey][zKey][:]
+    return data
+
+def readAsc(filename, handler=None):
+    if not handler:
+        handler = open(filename, 'rb')
+
+    handler.close()
+    return rows
+
+def readFile(fn, package=None, params={}):
+    fileHandler = None
+    tempFile = None
+    ext = fn.split(".")[-1].lower()
+    # check for .zip
+    if not os.path.isfile(fn) and package:
+        zf = zipfile.ZipFile(package, 'r')
+        # special case: NetCDF cannot be read via filehandler; must extract
+        if ext == "nc":
+            zf.extract(fn)
+            tempFile = fn
+        else:
+            fileHandler = zf.open(fn)
+    # check for .gz
+    elif os.path.isfile(fn) and ext == "gz":
+        fileHandler = gzip.open(fn, 'rb')
+        ext = fn.split(".")[-2].lower()
+    elif not os.path.isfile(fn):
+        print("Cannot find file %s" % fn)
+        return False
+
+    results = None
+    if ext == "csv":
+        results = readCsv(fn, handler=fileHandler)
+    elif ext == "nc":
+        results = readNetCDF(fn, params=params)
+    else:
+        results = readTxt(fn, handler=fileHandler)
+
+    if tempFile:
+        os.remove(tempFile)
+    return results
---
+++ processData.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import glob
+import json
+from lib import ascDump, readFile, ncDump
+import os
+import numpy as np
+from pprint import pprint
+import sys
+
+# input
+parser = argparse.ArgumentParser()
+parser.add_argument('-config', dest="CONFIG", default="config.json", help="Input config file")
+parser.add_argument('-in', dest="INPUT_DIR", default="data/downloads/", help="Input directory")
+parser.add_argument('-res', dest="RESOLUTION", default=1.0, type=float, help="Resolution in degrees latitude")
+parser.add_argument('-points', dest="POINTS", default=800, type=int, help="Target data points")
+parser.add_argument('-out', dest="OUTPUT_DIR", default="data/", help="Output directory")
+parser.add_argument('-overwrite', dest="OVERWRITE", default=0, type=int, help="Overwrite existing data?")
+args = parser.parse_args()
+
+# Parse arguments
+CONFIG = args.CONFIG
+INPUT_DIR = args.INPUT_DIR
+RESOLUTION = args.RESOLUTION
+POINTS = args.POINTS
+OUTPUT_DIR = args.OUTPUT_DIR
+OVERWRITE = args.OVERWRITE > 0
+
+# ncDump("data/downloads/gpw_v4_une_atotpopbt_dens_2pt5_min.nc")
+# ascDump("data/downloads/gpw_v4_national_identifier_grid_rev10_2pt5_min.asc")
+# sys.exit()
+
+# Make sure output dir exist
+outDir = os.path.dirname(OUTPUT_DIR)
+if not os.path.exists(outDir):
+    os.makedirs(outDir)
+
+files = []
+with open(CONFIG) as f:
+    configData = json.load(f)
+    files = configData["files"]
+
+for f in files:
+    outFile = OUTPUT_DIR + f["id"] + ".json"
+    data = None
+    filename = f["filename"]
+    # Check if file exists already
+    if os.path.isfile(outFile) and not OVERWRITE:
+        print("%s already exists. Skipping." % outFile)
+    else:
+        package = None if "package" not in f else f["package"]
+        params = {} if "params" not in f else f["params"]
+        if "*" in filename:
+            filenames = glob.glob(filename)
+            datas = []
+            for fn in filenames:
+                datas.append(readFile(fn, package=package, params=params))
+            # TODO: get mean of data
+        else:
+            data = readFile(f["filename"], package=package, params=params)
+    if data:
+        # Write to file
+        with open(outFile, 'w') as fout:
+            f["data"] = data
+            json.dump(f, fout)
+            print("Wrote data to %s" % outFile)
