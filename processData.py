da7ff0c816ee44d1f70c6e2de56a8ea1194ab879
brianfoo <brian@youaremyjoy.org>
2018-10-03 21:28:02 -0400

    implement data output, plot

--- processData.py
+++ processData.py
@@ -5,6 +5,7 @@ import glob
 import json
 from lib import ascDump, ncDump, norm, readFile
 import math
+from matplotlib import pyplot as plt
 import os
 import numpy as np
 from PIL import Image
@@ -20,6 +21,7 @@ parser.add_argument('-points', dest="POINTS", default=800, type=int, help="Targe
 parser.add_argument('-out', dest="OUTPUT_DIR", default="data/", help="Output directory")
 parser.add_argument('-overwrite', dest="OVERWRITE", default=0, type=int, help="Overwrite existing data?")
 parser.add_argument('-draw', dest="DRAW", default=0, type=int, help="Draw data files?")
+parser.add_argument('-plot', dest="PLOT", default=0, type=int, help="Plot data?")
 args = parser.parse_args()
 
 # Parse arguments
@@ -30,6 +32,7 @@ POINTS = args.POINTS
 OUTPUT_DIR = args.OUTPUT_DIR
 OVERWRITE = args.OVERWRITE > 0
 DRAW = args.DRAW > 0
+PLOT = args.PLOT > 0
 
 # ncDump("data/downloads/gpw_v4_une_atotpopbt_dens_2pt5_min.nc")
 # ascDump("data/downloads/gpw_v4_national_identifier_grid_rev10_2pt5_min.asc")
@@ -46,6 +49,35 @@ with open(CONFIG) as f:
     configData = json.load(f)
     files = configData["files"]
 
+def getLatitudeData(data, resolution, points, mode="mean"):
+    height = resolution / 180.0
+    results = []
+    h, w = data.shape
+    for i in range(points):
+        progress = 1.0 * i / (points-1)
+        start = progress * (1.0-height)
+        end = start + height
+        start = int(round(start * h))
+        end = int(round(end * h))
+        subset = data[start:end]
+        subset = subset.reshape(-1)
+        count = len(subset)
+        subset = subset[~np.isnan(subset)]
+        result = None
+        if mode == "sum":
+            result = int(np.sum(subset))
+        elif mode == "set":
+            result = list(np.unique(subset.astype(int)))
+        elif mode == "percent":
+            result = 1.0 * len(subset) / count
+        else:
+            result = np.mean(subset)
+            if np.isnan(result):
+                result = 0
+            result = int(result)
+        results.append(result)
+    return results
+
 def valueToColor(value, minValue, maxValue):
     if np.isnan(value):
         return (255, 0, 0)
@@ -61,22 +93,6 @@ def drawData(d, filename):
     if os.path.isfile(filename):
         return False
     print("Processing %s..." % filename)
-
-    # from matplotlib import pyplot as plt
-    # plt.figure(figsize = (20,10))
-    # x = d.reshape(-1)
-    # x = x[~np.isnan(x)]
-    # x = x[x > 1000]
-    # # example data
-    # mu = 100  # mean of distribution
-    # sigma = 15  # standard deviation of distribution
-    # num_bins = 100
-    # fig, ax = plt.subplots()
-    # # the histogram of the data
-    # n, bins, patches = ax.hist(x, num_bins)
-    # plt.show()
-    # return False
-
     minValue = np.nanmin(d)
     maxValue = np.nanmax(d)
     print("Value range: %s - %s" % (minValue, maxValue))
@@ -88,6 +104,16 @@ def drawData(d, filename):
     im = Image.fromarray(pixels.astype('uint8'), 'RGB')
     print("Saving %s..." % filename)
     im.save(filename)
+    print("Saved %s" % filename)
+
+def drawPlot(y, filename):
+    if os.path.isfile(filename):
+        return False
+    plt.figure(figsize=(20,10))
+    x = np.linspace(90, -90, len(y))
+    plt.plot(x, y)
+    plt.savefig(filename)
+    plt.close()
     print("Saved %s" % filename)
 
 # files = [files[4]]
@@ -116,10 +142,15 @@ for f in files:
             data = readFile(filename, package=package, params=params)
 
     if DRAW:
-        drawData(data, "output/" + f["id"] + ".png")
-    # if data is not None:
-        # # Write to file
-        # with open(outFile, 'w') as fout:
-        #     f["data"] = data
-        #     json.dump(f, fout)
-        #     print("Wrote data to %s" % outFile)
+        drawData(data, "output/map_" + f["id"] + ".png")
+
+    if data is not None:
+        # Write to file
+        latitudeData = getLatitudeData(data, RESOLUTION, POINTS, mode=f["reduceMode"])
+        if PLOT and f["reduceMode"] != "set":
+            drawPlot(latitudeData, "output/plot_" + f["id"] + ".png")
+
+        with open(outFile, 'w') as fout:
+            f["data"] = latitudeData
+            json.dump(f, fout)
+            print("Wrote data to %s" % outFile)
