816d37718fbb6a97d70aace471cc5e26a03c7cac
brianfoo <brian@youaremyjoy.org>
2018-10-05 18:26:12 -0400

    implement latitude on scroll

--- ui/js/app.js
+++ ui/js/app.js
@@ -8,8 +8,51 @@ var App = (function() {
     this.init();
   }
 
-  App.prototype.init = function(){
+  function lerp(a, b, percent) {
+    return (1.0*b - a) * percent + a;
+  }
 
+  function round(value, precision) {
+    return +value.toFixed(precision);
+  }
+
+  App.prototype.init = function(){
+    this.$highlight = $("#map-highlight");
+    this.$label = $("#map-label");
+
+    this.onResize();
+    this.loadListeners();
+  };
+
+  App.prototype.loadListeners = function(){
+    var _this = this;
+
+    $(window).on("resize", function(e){
+      _this.onResize();
+    })
+
+    $(window).on("scroll", function(e) {
+      _this.onScroll();
+    });
+  };
+
+  App.prototype.onResize = function(){
+    this.windowDelta = $(document).height() - $(window).height();
+    this.onScroll();
+  };
+
+  App.prototype.onScroll = function(){
+    var scrollPercent = $(window).scrollTop() / this.windowDelta;
+
+    var lat = lerp(90, -90, scrollPercent);
+    lat = round(lat, 1);
+
+    // update label
+    if (lat < 0) lat = -lat + "°S";
+    else if (lat > 0) lat += "°N";
+    else lat += "°";
+
+    this.$label.text(lat);
   };
 
   return App;
