fd91c2326b7e141554e234f21d4d881cca49ba7e
brianfoo <brian@youaremyjoy.org>
2018-10-05 15:14:11 -0400

    ui boilerplate

---
+++ ui/js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var App = (function() {
+
+  function App(config) {
+    var defaults = {};
+    this.opt = _.extend({}, defaults, config);
+    this.init();
+  }
+
+  App.prototype.init = function(){
+
+  };
+
+  return App;
+
+})();
+
+$(function() {
+  var app = new App({});
+});
